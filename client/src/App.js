a21ec72f86627a4935fe10d7a72b71ec551ad443
AlexanderLiu <alexanderliu2016@gmail.com>
2023-03-11 11:54:20 -0800

    update app.js: create search for test get

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  function search() {
+    axios.get("core/users/", {"data": "test"})
+      .then(response  => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +29,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={search}>Click</button>
       </header>
     </div>
   );
