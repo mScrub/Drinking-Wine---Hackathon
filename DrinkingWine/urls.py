d443f992e069e62f7334cbc088f4dd8430914e22
AlexanderLiu <alexanderliu2016@gmail.com>
2023-03-11 12:15:03 -0800

    update urls: add path to core urls

--- DrinkingWine/urls.py
+++ DrinkingWine/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import front
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('core/', include('core.urls')),
     path("", front, name="front"),
 ]
