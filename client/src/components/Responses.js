94002bf4bd40bab1d099640cda91e17a05e75cc3
AlexanderLiu <alexanderliu2016@gmail.com>
2023-03-11 22:14:25 -0800

    add new line

--- client/src/components/Responses.js
+++ client/src/components/Responses.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Response = ({ content, user }) => {
-  return <div className={user ? "inset-y-0 right-0 w-3/4" : ""}>{content}</div>;
+  return (
+    <>
+      <div className={user ? "inset-y-0 right-0 w-3/4" : ""}>{content}</div>
+      <br />
+    </>
+    );
 };
 
 export default Response;
