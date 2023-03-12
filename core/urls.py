c6278f891ede0875692e24fc2b47007e2e46bbf8
AlexanderLiu <alexanderliu2016@gmail.com>
2023-03-11 19:11:07 -0800

    map the urls

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import front, users, create_user
+from .views import front, users, create_user, login, writing
 
 urlpatterns = [
     path('', front, name='front'),
     path('users/', users, name='users'),
-    path('create_user/', create_user, name='create_user')
+    path('create_user/', create_user, name='create_user'),
+    path('login/', login, name='login'),
+    path('writing/', writing, name='writing'),
 ]
