bdaccd45ad0067078070db9d79b54756de9c5fc0
AlexanderLiu <alexanderliu2016@gmail.com>
2023-03-11 21:03:28 -0800

    add mappng to chatgpt

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import front, users, create_user, login, writing, is_logged_in
+from .views import front, users, create_user, login, writing, is_logged_in, get_chatgpt_response
 
 urlpatterns = [
     path('', front, name='front'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('writing/', writing, name='writing'),
     path('is_logged_in/', is_logged_in, name='is_logged_in'),
+    path('get_chatgpt_response/', get_chatgpt_response, name='get_chatgpt_response'),
 ]
