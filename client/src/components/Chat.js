c06940363ee2d811902d77564dd4c4323421e7ab
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-03-11 17:54:22 -0800

    Added recording

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,10 +1,41 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 const Chat = () => {
   const { state } = useLocation();
-  console.log(state);
-  return <div>Chat</div>;
+  // Get data...
+  // console.log(state.position, state.ambiance);
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable,
+  } = useSpeechRecognition();
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true });
+
+  if (!browserSupportsSpeechRecognition || !isMicrophoneAvailable) {
+    return <h1> Browser does not support Speech to Text d </h1>;
+  }
+
+  return (
+    <div>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button
+        onTouchStart={startListening}
+        onMouseDown={startListening}
+        onTouchEnd={SpeechRecognition.stopListening}
+        onMouseUp={SpeechRecognition.stopListening}
+      >
+        Hold to talk
+      </button>
+      <p>{transcript}</p>
+    </div>
+  );
 };
 
 export default Chat;
