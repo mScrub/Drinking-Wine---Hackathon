4c83c913e429fa518968e1bb3c7617bd629e618d
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-03-11 19:08:48 -0800

    Added UI

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
+import Response from "./Responses";
+import { FaMicrophone } from "react-icons/fa";
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
   const { state } = useLocation();
+
   // Get data...
   // console.log(state.position, state.ambiance);
   const {
@@ -22,19 +26,58 @@ const Chat = () => {
     return <h1> Browser does not support Speech to Text d </h1>;
   }
 
+  const recording = () => {
+    if (listening) {
+      SpeechRecognition.stopListening();
+      storeTranscript();
+    } else {
+      SpeechRecognition.startListening({ continuous: true });
+    }
+  };
+
+  const storeTranscript = () => {
+    if (transcript === "") return;
+    setTimeout(() => {
+      setMessages((messages) => [...messages, transcript]);
+      SpeechRecognition.stopListening();
+    }, 1000);
+    resetTranscript();
+  };
+
   return (
-    <div>
-      <p>Microphone: {listening ? "on" : "off"}</p>
-      <button
-        onTouchStart={startListening}
-        onMouseDown={startListening}
-        onTouchEnd={SpeechRecognition.stopListening}
-        onMouseUp={SpeechRecognition.stopListening}
+    <>
+      <div>
+        <h1 className="py-3 text-center font-semibold ">
+          {" "}
+          Practice: {state.position}{" "}
+        </h1>
+        <div> {transcript} </div>
+        <div className="flex, flex-col">
+          <Response
+            content={
+              "Hello! Welcome to our office. It's great to meet you in person. I'm glad you made it here safely. Please have a seat and make yourself comfortable. Can I get you some water or coffee before we start?"
+            }
+            user={true}
+          />
+          {messages.map((message) => (
+            <Response key={message} content={message} />
+          ))}
+        </div>
+      </div>
+
+      {listening && (
+        <div className="w-full h-full inset-0 fixed bg-gray-700 opacity-60"></div>
+      )}
+      <div
+        onClick={recording}
+        className="flex flex-row justify-center item-center mb-3 fixed inset-x-0 bottom-0"
       >
-        Hold to talk
-      </button>
-      <p>{transcript}</p>
-    </div>
+        <FaMicrophone
+          size="50"
+          className="text-white bg-blue-900 p-3 rounded-full"
+        />
+      </div>
+    </>
   );
 };
 
---
+++ client/src/components/Responses.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Response = ({ content, user }) => {
+  return <div className={user ? "inset-y-0 right-0 w-3/4" : ""}>{content}</div>;
+};
+
+export default Response;
